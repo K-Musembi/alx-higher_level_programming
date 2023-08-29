093bd47534a862336944a8d7e7c8101434cf54a3
K-Musembi <122810744+K-Musembi@users.noreply.github.com>
2023-08-29 12:46:25 -0700

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+'''A module that defines a class with its attributes and methods.'''
+
 class Square:
     '''An empty class that defines a square, with no attributes or methods.
 
